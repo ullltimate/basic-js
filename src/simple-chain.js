ae262fd265ca703209d05ab384b040a83bf84be7
ullltimate <ullltimate.zp@gmail.com>
2022-10-16 19:08:00 +0300

    feat:solution chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+	chain : [], 
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+		return this.chain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+  	if (value === undefined){
+    	this.chain.push('()');
+    } else {
+    	this.chain.push('( '+value+' )');
+    }
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+  	if (position <= 0 || Number.isInteger(position) != true || position > this.chain.length || isNaN(position)){
+    	this.chain = [];
+    	throw new Error("You can\'t remove incorrect link!");
+    } else{
+    	this.chain.splice(position-1, 1);
+    }
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+		this.chain.reverse();
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    var result = this.chain.join('~~');
+    this.chain = [];
+		return result
   }
 };
 
